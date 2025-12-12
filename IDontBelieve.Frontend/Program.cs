f15d017369c35d2bd4223bb95691173e6cf546ee
Mikhail Petrachenka <77238687+aCrYoZPS@users.noreply.github.com>
2025-12-12 13:27:12 +0300

    improved navbar

--- IDontBelieve.Frontend/Program.cs
+++ IDontBelieve.Frontend/Program.cs
@@ -15,15 +15,14 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<GameHubService>();
 
-builder.Services.AddHttpClient("AuthHttpClient", client =>
-{
-    client.BaseAddress = new Uri(builder.Configuration["ApiBaseUrl"] ?? "http://localhost:5000");
-});
+builder.Services.AddHttpClient("AuthHttpClient",
+    client => { client.BaseAddress = new Uri(builder.Configuration["ApiBaseUrl"] ?? "http://localhost:5000"); });
 
 builder.Services.AddAuthorizationCore();
 builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
+builder.Services.AddScoped<CustomAuthStateProvider>();
 builder.Services.AddScoped<IAuthService, AuthService>();
-builder.Services.AddScoped<ILeaderboardHubService>(provider => 
+builder.Services.AddScoped<ILeaderboardHubService>(provider =>
     new LeaderboardHubService(
         provider.GetRequiredService<IJSRuntime>(),
         builder.Configuration["ApiBaseUrl"] ?? "http://localhost:5000"
